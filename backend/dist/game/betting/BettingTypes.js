407725c30385ea1310f297dc8fec9a0f8eb0ef57
gad.morgenstern@gmail.com <gad.morgenstern@gmail.com>
2025-02-11 01:48:43 +0200

    Romove dist from git

--- backend/dist/game/betting/BettingTypes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/game/betting/BettingTypes.ts - should be moved other places , TODO
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=BettingTypes.js.map
--- backend/dist/login/LoginEndpoint.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// src/login/LoginEndpoint.ts - Handles player login request
-//# sourceMappingURL=LoginEndpoint.js.map
